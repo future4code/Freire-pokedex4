afff803e9fe53a3266811e13ae18efc870adfaa2
mariawoo <graciavwoo@gmail.com>
2022-07-21 00:06:53 -0300

    botão ir para pokedex linkado no header ps: odeio botoes

--- pokedex/src/components/Header.js
+++ pokedex/src/components/Header.js
@@ -2,13 +2,22 @@ import React from "react";
 import pokemonlogo from "../assets/img/PokemonLogo.png"
 import { DivTop } from "./styled";
 import { Button } from "./styled";
+import { useNavigate } from "react-router-dom";
+
+
+
 
 export const Header = () => {
+    const navigate = useNavigate();
+    
+    const goToPokedex = () => {
+        navigate("/pokedex");
+      };
     
     return(
         <DivTop>
             <img src={pokemonlogo} alt="Pokemon Api Logo"/>
-            <Button>Pokédex</Button> 
+            <Button onClick= {goToPokedex} >Ir para Pokédex</Button> {/*botão não funciona */}
         </DivTop>
-    )
-}
+    );
+}; 
