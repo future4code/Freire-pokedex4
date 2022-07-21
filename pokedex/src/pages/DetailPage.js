a1001c719ddc286714969567bd12a703cc3600a1
mariawoo <graciavwoo@gmail.com>
2022-07-21 09:09:50 -0300

    arrumando cód que não sei oq mexi

--- pokedex/src/pages/DetailPage.js
+++ pokedex/src/pages/DetailPage.js
@@ -1,9 +1,5 @@
-
-import React, { useContext, useEffect } from "react";
-import { useNavigate, useParams } from "react-router-dom";
-import PokedexContext from "../PokedexContext";
-import { Background } from "./styled";
-
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export const DetailPage = () => {
   const navigate = useNavigate();
@@ -11,45 +7,10 @@ export const DetailPage = () => {
   const goToHome = () => {
     navigate("/");
   };
-
-  const { id } = useParams()
-
-  const { pokemons, removePokedex, addPokedex } = useContext(PokedexContext)
-
-  useEffect(() => {
-
-  }, [])
-
-
   return (
-    <Background>
+    <div>
       <p>Página de Detalhes</p>
-      <br />
       <button onClick={goToHome}>Home</button>
-
-      <br />
-      <br />
-      <br />
-      {!pokemons[id].inPokedex ? <button onClick={() => addPokedex(id)}>Adicionar à Pokedex</button> :
-        <button onClick={() => removePokedex(id)}>Remover da Pokedex</button>}
-      <br />
-      <img src={pokemons[id].img} width={150} height={150} />
-      <p>
-        {pokemons[id].name}
-      </p>
-      <p>{pokemons[id].skills.map(s => " " + s + " ")}</p>
-      <img src={pokemons[id].img_front} width={80} height={80} />
-      <img src={pokemons[id].img_back} width={80} height={80} />
-      <p>Base stats</p>
-      {pokemons[id].stats.map((s,i) => (
-        <span key={i}>
-          <p>{`${s[0]}: ${s[1]}`}</p>
-        </span>
-      ))}
-      <p>{`Total: ${pokemons[id].stats.map(s => s[1]).reduce((i,j) => i+j)}`}</p>
     </div>
-
-    </Background>
-
   );
 };
--- pokedex/src/pages/Home.js
+++ pokedex/src/pages/Home.js
@@ -1,63 +1,21 @@
-import React, { useContext } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
-
-import PokedexContext from "../PokedexContext";
-
-import { Background } from "./styled";
-import {  useRequestData } from "../Hooks/useRequestData";
-
 
 export const Home = () => {
   const navigate = useNavigate();
 
-  const goDetailPage = (index) => {
-    navigate(`/detail/${index}`);
+  const goToPokedex = () => {
+    navigate("/pokedex");
+  };
+  const goDetailPage = () => {
+    navigate("/detail");
   };
 
-  // const goToPokedex = () => {
-  //   navigate("/pokedex");
-  // };
-
-  const { pokemons, removePokedex, addPokedex } = useContext(PokedexContext)
-
-  const pkms = useRequestData();
-
-  // const allpkms = pkms.map((pkms,key) => <p key={key}>{pkms?.name}</p> )
-
-
-
-  
-
   return (
-    <Background>
-      {/* {allpkms} */}
+    <div>
       <p>Home</p>
-
-      <br />
-      <br />
-      {/* <button onClick={goToPokedex}>Ir para pokedex</button> mudar botao botão chato */}
-      <div>
-        {pokemons.map((p, index) => (
-          <span key={index}>
-            <img src={p.img} width={50} height={50} /> {/* imagem do pokemon*/}
-            <p>
-              {p.name}
-            </p>
-            <p>{p.skills.map(s => " " + s + " ")}</p> {/* map para skills dos pokemons*/}
-            {!p.inPokedex ? <button onClick={() => addPokedex(index)}>Adicionar à Pokedex</button> :
-              <button onClick={() => removePokedex(index)}>Remover da Pokedex</button>}
-              <button onClick={() => goDetailPage(index)}>página de detalhe</button>
-            <br />
-            <br />
-          </span>
-        ))}
-      </div>
-      
-    </div>
-
       <button onClick={goToPokedex}>ir para pokedex</button>
       <button onClick={goDetailPage}>página de detalhe</button>
-    </Background>
-
+    </div>
   );
 };
