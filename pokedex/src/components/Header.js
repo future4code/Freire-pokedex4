944ede566a855d19cfebf0417276e9c6b4aba317
Murilo Cardoso <murilo_mac@hotmail.com>
2022-07-21 12:29:02 -0300

    ajustes no header

--- pokedex/src/components/Header.js
+++ pokedex/src/components/Header.js
@@ -1,28 +1,23 @@
 import React from "react";
-import pokemonlogo from "../assets/img/PokemonLogo.png"
+import pokemonlogo from "../assets/img/PokemonLogo.png";
 import { useNavigate } from "react-router-dom";
 import { Button, ButtonNone, DivTop } from "./styled";
 
-
-
-
 export const Header = () => {
-    const navigate = useNavigate();
-    
-    const goToPokedex = () => {
-        navigate("/pokedex");
-      };
-    const goToHome = () => {
-        navigate("/");
-      };
-    
-    return(
-        <DivTop>
-            <ButtonNone></ButtonNone>
-            <img src={pokemonlogo} alt="Pokemon Api Logo"/>
-            <Button onClick= {goToPokedex} >Ir para Pokédex</Button> 
-            <Button onClick={goToHome}>Home</Button> 
-            
-        </DivTop>
-    );
-}; 
+  const navigate = useNavigate();
+
+  const goToPokedex = () => {
+    navigate("/pokedex");
+  };
+  const goToHome = () => {
+    navigate("/");
+  };
+
+  return (
+    <DivTop>
+      <Button onClick={goToHome}>Home</Button>
+      <img src={pokemonlogo} alt="Pokemon Api Logo" />
+      <Button onClick={goToPokedex}>Pokédex</Button>
+    </DivTop>
+  );
+};
--- pokedex/src/components/styled.js
+++ pokedex/src/components/styled.js
@@ -1,25 +1,23 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const DivTop = styled.div`
-
-background-color: white;
-display: flex;
-justify-content: space-between;
-padding: 15px;
-align-items: center;
-`
+  background-color: white;
+  display: flex;
+  justify-content: space-between;
+  padding: 15px;
+  align-items: center;
+`;
 
 export const Button = styled.button`
-
-
-border: none;
-border-radius: 8px;
-width: 200px;
-height: 60px;
-color: white;
-background-color:#33A4F5;
-font-size: 20px;
-`
-export const ButtonNone = styled.button`
-border: none;
-`
+  box-shadow: 2px 1px 7px #808080;
+  border: none;
+  border-radius: 8px;
+  width: 150px;
+  height: 40px;
+  color: white;
+  background-color: #33a4f5;
+  font-size: 20px;
+  cursor: pointer;
+  text-align: center;
+  font-size: 15px;
+`;
